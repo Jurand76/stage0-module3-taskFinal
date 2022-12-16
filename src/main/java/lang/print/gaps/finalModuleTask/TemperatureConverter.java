9deb8f31340a86ca0a1043e6f87c4579a6a73cc6
Jurand76 <jurand@interia.pl>
2022-12-16 21:45:31 +0100

    fix: code fixed

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         float result;
-        result = temperatureCelsius * 9 / 5 + 32;
+        result = (float)temperatureCelsius * 9 / 5 + 32;
         System.out.println(result);
     }
 }
