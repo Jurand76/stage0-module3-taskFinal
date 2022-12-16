3f1b9fc556445345ddee9caf96db6057ae7c2dcf
Jurand76 <jurand@interia.pl>
2022-12-16 21:43:27 +0100

    fix: code fixed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result, first, second, third, fourth;
+        first = number / 1000;
+        second = (number - first*1000) / 100;
+        third = (number - first*1000 - second*100) / 10;
+        fourth = number - first*1000 - second*100 - third*10;
+        System.out.println(first+second+third+fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first;
+        int second;
+        int third;
+        first = number / 100;
+        second = (number - (first*100)) / 10;
+        third = number - first*100 - second*10;
+        System.out.println(third * 100 + second * 10 + first);
     }
 }
